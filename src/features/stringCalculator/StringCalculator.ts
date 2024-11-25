0f4f1508f2766a231fc31b827b5a7ca9d1bd1e3b
shuklaksh <shuklaksh156@gmail.com>
2024-11-25 23:32:13 +0530

    code refactor

--- src/features/stringCalculator/StringCalculator.ts
+++ src/features/stringCalculator/StringCalculator.ts
@@ -1,11 +1,7 @@
-function addNumbers(numbers: string){
-    if(numbers == "") return 0;
-    const numbersArray = numbers.split(",");
-    let sum = 0;
-    for(let i = 0; i < numbersArray.length; i++) {
-        sum += Number(numbersArray[i]);
-    }
-    return sum;
-}
-
-export default addNumbers;
+const addNumbers = (numbers: string): number => 
+    numbers
+      .split(',')
+      .reduce((sum, num) => sum + Number(num), 0);
+  
+  export default addNumbers;
+  
